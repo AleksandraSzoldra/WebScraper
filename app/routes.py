d9c98b2986e8aea7e921833f8cd4146ea4318a92
s-301-13 <s-301-13@gumiforest.uek.krakow.pl>
2022-05-31 14:46:42 +0200

    31.05.2022

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,7 @@ from app import app
 from flask import render_template
 
 @app.route('/')
+@app.route('/index')
 @app.route('/index/<name>')
 def index(name="Hello World"):
-   return render_template('index.html', text=name)
-
-@app.route('/something')
-def something():
-    return 'Aleksandra Szoldra' 
+    return render_template("index.html.jinja", text=name)
