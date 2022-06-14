1c22bc811b8314e9156e0288d94e50b4c5f36e04
s-301-13 <s-301-13@gumiforest.uek.krakow.pl>
2022-06-14 14:44:07 +0200

    14-06.2022 ASz

---
+++ app/models/opinion.py
@@ -0,0 +1,45 @@
+from app.utils import get_item
+from app.parameters import selectors
+
+class Opinion:
+    def __init__(self, author="", recommendation=None, stars=0, content="", useful=0, useless=0, publish_date=None, purchase_date=None, pros=[], cons=[], opinion_id=""):
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
+        self.pros = pros
+        self.cons = cons
+        self.opinion_id = opinion_id
+        return self
+    
+   def __str__(self):
+        return f"{self.author}, {self.recommendation}, {self.stars}, {self.content}, {self.useful}, {self.useless}, {self.publish_date}, {self.purchase_date}, {self.pros}, {self.cons}, {self.opinion_id}"
+
+    def __repr__(self): #kopia obiektu
+        return f"{self.author}, {self.recommendation}, {self.stars}, {self.content}, {self.useful}, {self.useless}, {self.publish_date}, {self.purchase_date}, {self.pros}, {self.cons}, {self.opinion_id}"
+    
+    def to_dict(self):
+        opinion_data ={
+            "author" : self.author,
+            "recommendation" : self.recommendation,
+            "stars" : self.stars, 
+            "content" : self.content,
+            "useful" : self.useful,
+            "useless" : self.useless,
+            "publish_date" : self.publish_date,
+            "purchase_date" : self.purchase_date,
+            "pros" : self.pros,
+            "cons" : self.cons,
+            "opinion_id" : self.opinion_id
+        }
+        return opinion_data
+
+    def extract_opinion(self, opinion):
+        for key, value in selectors.items():
+            setattr(self, key, get_item(opinion, *value))
+        self.opinion_id = opinion["data-entry-id"]             
+        return self
--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,75 @@
-    class Product():
-        def __init__(): 
-            praca domowa : opinia i produkt 
+import requests
+import os
+import json
+import pandas as pd 
+from bs4 import BeautifulSoup
+from app.utils import get_item
+from app.models.opinion import Opinion
+
+class Product:
+    def __init__(self, product_id=0, opinions=[], product_name="", opinions_count=0, pros_count=0, cons_count=0, average_score=0):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.opinions = opinions
+        self.opinions_count = opinions_count
+        self.pros_count = pros_count
+        self.cons_count = cons_count
+        self.average_score = average_score
+        return self
+    
+  def __str__(self):
+        return f"Our Product's data: {self.product_id}, {self.product_name}, {self.opinions}, {self.opinions_count}, {self.pros_count}, {self.cons_count}, {self.average_score}"
+
+    def __repr__(self):
+        return f"Our Product's data: {self.product_id}, {self.product_name}, {self.opinions}, {self.opinions_count}, {self.pros_count}, {self.cons_count}, {self.average_score}"
+
+    def to_dict(self):
+        product_data = {
+            "product_id": self.product_id,
+            "product_name": self.product_name,
+            "opinions": self.opinions,
+            "opinions_count": self.opinions_count,
+            "pros_count": self.pros_count,
+            "cons_count": self.cons_count,
+            "average_score": self.average_score,
+        }
+        return product_data
+
+    def extract_product(self):
+        url = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        response = requests.get(url)
+        page = BeautifulSoup(response.text, 'html.parser')
+        self.product_name = get_item(page, "h1.product-top__product-info__name")
+        while(url):
+            response = requests.get(url)
+            page = BeautifulSoup(response.text, 'html.parser')
+            opinions = page.select("div.js_product-review")
+            for opinion in opinions:
+                self.opinions.append(Opinion().extract_opinion(opinion))
+            try:    
+                url = "https://www.ceneo.pl"+get_item(page,"a.pagination__next","href")
+            except TypeError:
+                url = None
+    
+    def process_stats(self):
+        opinions = pd.read_json(json.dumps([opinion.to_dict() for opinion in self.opinions])) 
+        self.opinions_count = len(self.opinions.index),
+        self.pros_count = opinions.pros.map(bool).sum()
+        self.cons_count = opinions.cons.map(bool).sum()
+        self.average_score = opinions.stars.mean().round(2)
+        return self
+    
+
+
+
+    def save_opinions(self):        
+        if not os.path.exists("app/opinions"):
+            os.makedirs("app/opinions")
+        with open(f"app/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+            json.dump(self.to_dict, jf, indent=4, ensure_ascii=False)
+    
+    def save_stats(self):        
+        if not os.path.exists("app/products"):
+            os.makedirs("app/products")
+        with open(f"app/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
+            json.dump(self.opinions, jf, indent=4, ensure_ascii=False)
